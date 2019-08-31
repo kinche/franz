e1c7648544504d04b84c97b26f9bb8f94cad90bf
bukinoshita <bukinoshita@gmail.com>
2019-08-31 17:18:14 -0300

    feat: add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,52 @@
 // Packages
 import React, { Component } from 'react'
 
+// Layouts
+import Page from '../layouts/page'
+
 class Home extends Component {
   render() {
-    return <h1>franz</h1>
+    return (
+      <Page>
+        <main>
+          <div className="content">
+            <div className="content-left">
+              <h1>
+                franz is the first
+                <br />
+                benchmark analytics.
+              </h1>
+            </div>
+
+            <div className="content-right">
+              <img src="static/product.png" />
+            </div>
+          </div>
+        </main>
+
+        <style jsx={true}>{`
+          main {
+            padding: 150px;
+            display: flex;
+            align-items: center;
+            height: 100vh;
+          }
+
+          .content {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            width: 100%;
+          }
+
+          h1 {
+            font-size: 50px;
+            font-weight: 400;
+            color: #222;
+          }
+        `}</style>
+      </Page>
+    )
   }
 }
 
