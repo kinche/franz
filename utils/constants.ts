f8fff26675f527e07a930220e336fde97e70badc
Bu Kinoshita <bukinoshita@gmail.com>
2019-09-11 13:28:45 -0300

    chore: add constants utils (#26)

---
+++ utils/constants.ts
@@ -0,0 +1,6 @@
+export const APPEARANCE_PRIMARY = 'primary'
+export const APPEARANCE_PRIMARY_SUBTLE = 'primary-subtle'
+
+export const SIZE_LARGE = 'large'
+export const SIZE_MEDIUM = 'medium'
+export const SIZE_SMALL = 'small'
