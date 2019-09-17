9482f7e49dbb6c706391f8ea2b59c33ff96c7350
Bu Kinoshita <bukinoshita@gmail.com>
2019-09-17 19:59:54 -0300

    feat: add redirect url on sign in (#34)
    * feat: add redirect url on sign in
    
    * fix: env vars

---
+++ utils/cookies/save-cookie.ts
@@ -0,0 +1,14 @@
+// Packages
+import { NextPageContext } from 'next'
+import { setCookie } from 'nookies'
+
+export function saveCookie(name: string, value: any, ctx?: NextPageContext, options = {}) {
+  const date = new Date()
+  const oneDay = date.setDate(date.getDate() + 1)
+  const cookieOptions = Object.assign(
+    {},
+    { maxAge: oneDay, path: '/', domain: process.env.KINCHE_COOKIES_DOMAIN },
+    options
+  )
+  setCookie(ctx, name, value, cookieOptions)
+}
